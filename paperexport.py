784cd47180d66e4785c635221922cc5cd676d9a0
Gregory Simonian <simonian@astronomy.ohio-state.edu>
2018-02-05 10:23:58 -0500

    Added some targeting information to figure 1.

--- paperexport.py
+++ paperexport.py
@@ -17,6 +17,8 @@ import astropy_util as au
 import catalog
 import sed
 import data_splitting as split
+import biovis_colors as bc
+import aspcap_corrections as aspcor
 
 PAPER_PATH = paths.HOME_DIR / "papers" / "rotation17"
 TABLE_PATH = PAPER_PATH / "tables"
@@ -39,7 +41,7 @@ def asteroseismic_data_splitter():
 def dwarf_data_splitter():
     '''Create a persistent DataSplitter for the cool dwarf sample.'''
     apogee = split.APOGEESplitter()
-    split.initialize_apogee_dwarf_rotation_sample(apogee)
+    apogee = split.initialize_cool_dwarfs(apogee)
     return apogee
 
 def get_asteroseismic_dwarfs():
@@ -68,12 +70,36 @@ def get_cool_sample():
     '''Get the final sample of cool dwarfs.
     
     Note that these include those without McQuillan detections.'''
-    apogeesplitter = dwarf_data_splitter()
+    splitter = dwarf_data_splitter()
 
-    # I want the targeting dwarfs to be just those that have APOGEE targeting
-    # flags. I also just want the targets with 5450 K > Teff > 4250 K.
-    apotargs = apogeesplitter.subsample(["Good Teff", "Cold", "~Bad"])
-    return apogeesplitter
+    # These are all the targets which were observed with APOGEE1
+    apogee1_samp = get_cool_apogee1_sample(splitter)
+    # Now get the targets which were observed in APOGEE2 that meet Jen's
+    # selection criteria: KIC log(g) > 4.0, Pinsonneault SDSS Teff < 5500 K,
+    # and 7 < H < 11.
+    apogee2_samp = splitter.subsample([
+        "APOGEE2_APOKASC_DWARF", "Jen Dwarf", "H Jen", "Jen Cool"])
+    # There were a bunch of targets which did not have SDSS Teffs because they
+    # were outside of the calibration region. Include the very cool ones.
+    nosdss_samp = splitter.subsample([
+        "APOGEE2_APOKASC_DWARF", "Jen Dwarf", "H Jen", "No SDSS Teff", 
+        "KIC Jen Cool"])
+
+    fullcool = vstack([apogee1_samp, apogee2_samp, nosdss_samp])
+
+    return fullcool
+
+def get_cool_apogee1_sample(splitter):
+    '''Get the subset of Jen's sample that were observed in APOGEE1.
+
+    This essentially selects based on the targeting flags in apogeesplitter to
+    check for APOGEE_KEPLER_COOLDWARF.'''
+    apogee1_samp = splitter.subsample(["APOGEE_KEPLER_COOLDWARF"])
+    return apogee1_samp
+
+def get_cool_apogee2_sample(apogeesplitter):
+    '''Get the subset of Jen's sample observed in APOGEE2.'''
+    pass
 
 def targeting_figure(dest=build_filepath(FIGURE_PATH, "targeting", "pdf")):
     '''Create figure showing where the two samples lie in the HR diagram.
@@ -82,7 +108,16 @@ def targeting_figure(dest=build_filepath(FIGURE_PATH, "targeting", "pdf")):
     asteroseismic = get_asteroseismic_dwarfs()
     cooldwarfs = get_cool_sample()
 
+    hr.logg_teff_plot(
+        asteroseismic["TEFF_COR"], asteroseismic["LOGG_FIT"], color=bc.blue,
+        marker="*", markersize=9, linestyle="")
+    hr.logg_teff_plot(
+        cooldwarfs["TEFF"], cooldwarfs["FPARAM"][:,1], color=bc.red,
+        marker="o", linestyle="")
+    plt.xlim(7000, 3500)
+    plt.ylim(5.0, 0.0)
 
+    plt.savefig(str(dest))
 
 
 if __name__ == "__main__":
@@ -99,10 +134,8 @@ looking for tidally-synchronized binaries in the Kepler field."""
     args = parser.parse_args()
 
     figlist = {
-        "hrsample": create_sample_HR_diagram,
-        "pspacesample": create_sample_Prot_diagram,
-        "obssampletable": create_observing_sample_table,
-        "apotable": create_APOGEE_table}
+        "targeting": targeting_figure
+    }
 
     genfigs = args.figs
     if not genfigs:
